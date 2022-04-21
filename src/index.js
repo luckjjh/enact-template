ea3febabb30a96870b532104872d8d1bb0f35803
bongseok13.choi <bongseok13.choi@lge.com>
2022-04-21 10:36:34 +0900

    Migration to enact 4.5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {render} from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
 
@@ -24,7 +24,8 @@ let appElement = (
 );
 
 if (isBrowser()) {
-	render(appElement, document.getElementById('root'));
+	const root = createRoot(document.getElementById('root'));
+	root.render(appElement);
 	appElement = null;
 }
 
